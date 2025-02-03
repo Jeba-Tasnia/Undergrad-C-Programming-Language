71e5682fe96002008dd4d6bb5b350ce2ff4aa725
Jeba-Tasnia <jebatasnia1971@gmail.com>
2025-02-03 22:44:32 +0600

    Oct to Deci

---
+++ Basic C Programme/07-OctToDesi.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    /*int num1;
+    printf("Enter any decimal number: ");
+    scanf("%d",&num1);
+    printf("The Octal number is: %o",num1);*/
+    int num2;
+    printf("Enter any octal number: ");
+    scanf("%o",&num2);
+    printf("The decimal number is: %d",num2);
+    return 0;
+
+}
