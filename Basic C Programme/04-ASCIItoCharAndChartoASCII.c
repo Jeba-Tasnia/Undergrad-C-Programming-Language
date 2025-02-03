aab8677e1d6b7a4e3e958538bef6e6e13f1ada5d
Jeba-Tasnia <jebatasnia1971@gmail.com>
2025-02-03 15:49:19 +0600

    ASCII to char and vice versa

---
+++ Basic C Programme/04-ASCIItoCharAndChartoASCII.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter any ASCII value: ");
+    scanf("%d",&n);
+    printf("The character value is: %c\n",n);
+    char ch;
+    printf("Enter any Character value: ");
+    scanf("%c",&ch);
+    printf("The ASCII value is: %d",ch);
+    return 0;
+}
