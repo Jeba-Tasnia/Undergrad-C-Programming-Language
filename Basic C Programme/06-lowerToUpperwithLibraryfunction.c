4fd861926c6b5fa05da41b7f83b9df39935202e4
Jeba-Tasnia <jebatasnia1971@gmail.com>
2025-02-03 21:30:02 +0600

    Upper to lower

--- Basic C Programme/06-lowerToUpperwithLibraryfunction.c
+++ Basic C Programme/06-lowerToUpperwithLibraryfunction.c
@@ -5,5 +5,11 @@ int main(){
     scanf("%c",&lower);
     char upper =toupper(lower);
     printf("The upper character is : %c",upper);
+    char upper;
+    printf("Enter any upper character: ");
+    scanf("%c",&upper);
+    char lower=tolower(upper);
+    printf("The character is : %c",lower);
+    return 0;
 
 }
